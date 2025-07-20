5e57dacea6a65f4829c7b608c65e4732a67cd97e
Nidhi Jha <nidhi.jha@se.com>
2025-07-20 17:54:40 +0530

    updated script

--- scripts/send_whatsapp.py
+++ scripts/send_whatsapp.py
@@ -1,5 +1,7 @@
 from twilio.rest import Client
 import os
+import pandas as pd
+
 
 # Load credentials from environment variables
 account_sid = os.environ['ACCOUNT_SID']
@@ -9,8 +11,18 @@ to_whatsapp_number = os.environ['TO_WHATSAPP_NO']
 
 client = Client(account_sid, auth_token)
 
+def read_csv(file_path):
+    """Read a CSV file and return its content."""
+
+    df = pd.read_csv(file_path)
+    symbols = df['Symbol'].tolist()
+    return_str = '\n'.join(symbols)
+    print(return_str)
+    return return_str
+
+
 message = client.messages.create(
-    body="Work in progress! you will get the updated information soon. Have a good day sir!",
+    body=read_csv("static/stock.csv"),
     from_=f'whatsapp:{from_whatsapp_number}',
     to=f'whatsapp:{to_whatsapp_number}'
 )
